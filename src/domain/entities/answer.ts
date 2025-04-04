a94c4a5171f8a4c36265a34fc1c9229a09936751
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-03 22:52:37 -0300

    refactor: abstracts entity creation | fix: vitest resolving path aliases

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,13 +1,32 @@
+import type { Optional } from '@/core/@types/optional'
 import { Entity } from '@/core/entities/entity'
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 interface AnswerProps {
+	questionId: UniqueEntityId
+	authorId: UniqueEntityId
 	content: string
-	authorId: string
-	questionId: string
+	createdAt: Date
+	updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
 	get content() {
 		return this.props.content
+	}
+
+	static create(
+		props: Optional<AnswerProps, 'createdAt'>,
+		id?: UniqueEntityId,
+	) {
+		const answer = new Answer(
+			{
+				...props,
+				createdAt: new Date(),
+			},
+			id,
+		)
+
+		return answer
 	}
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,31 @@
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import type { Slug } from './value-object/slug'
 import { Entity } from '@/core/entities/entity'
+import type { Optional } from '@/core/@types/optional'
 
 interface QuestionProps {
+	authorId: UniqueEntityId
+	bestAnswerId?: UniqueEntityId
 	title: string
 	content: string
-	authorId: string
 	slug: Slug
+	createdAt: Date
+	updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+	static create(
+		props: Optional<QuestionProps, 'createdAt'>,
+		id?: UniqueEntityId,
+	) {
+		const question = new Question(
+			{
+				...props,
+				createdAt: new Date(),
+			},
+			id,
+		)
+
+		return question
+	}
+}
