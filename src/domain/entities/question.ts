9bbf2eb441850ddd4c8eb5c923aee21ce88c5c86
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-03 21:32:41 -0300

    refactor: adds question slug field | feat: slug value-object for seo | test: slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,11 @@
 import { randomUUID } from 'node:crypto'
+import type { Slug } from './value-object/slug'
 
 interface QuestionProps {
 	title: string
 	content: string
 	authorId: string
+	slug: Slug
 }
 
 export class Question {
@@ -11,11 +13,13 @@ export class Question {
 	public title: string
 	public content: string
 	public authorId: string
+	public slug: Slug
 
 	constructor(props: QuestionProps, id?: string) {
 		this.id = id ?? randomUUID()
 		this.title = props.title
 		this.content = props.content
 		this.authorId = props.authorId
+		this.slug = props.slug
 	}
 }
---
+++ src/domain/entities/value-object/slug.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { Slug } from './slug'
+
+describe('Slug Value-object', () => {
+	it('should create a slug based on a received text', () => {
+		const slug = Slug.createFromText('An example question title')
+
+		expect(slug.value).toEqual('an-example-question-title')
+	})
+})
