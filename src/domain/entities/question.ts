468f191f4da9e6cb7cbfc920bc42e301a983bbc1
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-03 21:04:08 -0300

    refactor: improves entities fields | chore: adds answers repository interface | test: mocks an answers repository

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from 'node:crypto'
 
+interface QuestionProps {
+	title: string
+	content: string
+	authorId: string
+}
+
 export class Question {
 	public id: string
 	public title: string
 	public content: string
+	public authorId: string
 
-	constructor(title: string, content: string, id?: string) {
+	constructor(props: QuestionProps, id?: string) {
 		this.id = id ?? randomUUID()
-		this.title = title
-		this.content = content
+		this.title = props.title
+		this.content = props.content
+		this.authorId = props.authorId
 	}
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,16 +1,24 @@
-import { describe, it, expect } from "vitest";
-import { AnswerQuestionUseCase } from "./answer-question";
+import { describe, it, expect } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import type { AnswersRepository } from '../repositories/answers-repository'
+import type { Answer } from '../entities/answer'
+
+const fakeAnswersRepository: AnswersRepository = {
+	create: async (_: Answer) => {
+		return
+	},
+}
 
 describe('Answer Question Use Case', () => {
-  it('should create and answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+	it('should create and answer', async () => {
+		const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-    const answer = answerQuestion.execute({
-      questionId: '1',
-      instructorId: '1',
-      content: 'Test Answer'
-    })
+		const answer = await answerQuestion.execute({
+			questionId: '1',
+			instructorId: '1',
+			content: 'Test Answer',
+		})
 
-    expect(answer.content).toEqual('Test Answer')
-  })
+		expect(answer.content).toEqual('Test Answer')
+	})
 })
