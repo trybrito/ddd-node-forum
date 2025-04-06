cd740d6a27357ddeba771bc2a54112f20631d198
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-05 23:07:47 -0300

    refactor: using mocked data

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryQuestionsRepository } from 'tests/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { makeQuestion } from 'tests/factories/make-question'
+import { Slug } from '../../enterprise/entities/value-object/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -12,10 +13,12 @@ describe('Get Question By Slug', () => {
 	})
 
 	it('should be able to get a question by its slug', async () => {
-		const newQuestion = makeQuestion()
+		const newQuestion = makeQuestion({
+			slug: Slug.create('example-content'),
+		})
 		await inMemoryQuestionsRepository.create(newQuestion)
 
-		const { question } = await sut.execute({ slug: 'new-question' })
+		const { question } = await sut.execute({ slug: 'example-content' })
 
 		expect(question.id).toEqual(newQuestion.id)
 	})
