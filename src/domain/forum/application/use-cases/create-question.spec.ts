6a71e47a56fd452f2e6c12063648c9c8c56684e9
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-10 23:04:56 -0300

    feat: creating questions with attachments | refactor(test): create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'tests/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -15,9 +16,21 @@ describe('Create Question', () => {
 			authorId: '1',
 			title: 'New Question',
 			content: 'Test question',
+			attachmentsIds: ['1', '2'],
 		})
 
 		expect(result.isRight()).toBe(true)
 		expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value.question)
+		expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+		expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+			expect.objectContaining({
+				attachmentId: new UniqueEntityId('1'),
+				questionId: result.value.question.id,
+			}),
+			expect.objectContaining({
+				attachmentId: new UniqueEntityId('2'),
+				questionId: result.value.question.id,
+			}),
+		])
 	})
 })
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,6 +3,7 @@ import { Slug } from './value-object/slug'
 import type { Optional } from '@/core/@types/optional'
 import dayjs from 'dayjs'
 import { AggregateRoot } from '@/core/entities/aggregate-root'
+import type { QuestionAttachment } from './question-attachment'
 
 export interface QuestionProps {
 	authorId: UniqueEntityId
@@ -10,6 +11,7 @@ export interface QuestionProps {
 	title: string
 	content: string
 	slug: Slug
+	attachments: QuestionAttachment[]
 	createdAt: Date
 	updatedAt?: Date
 }
@@ -33,6 +35,10 @@ export class Question extends AggregateRoot<QuestionProps> {
 
 	get slug() {
 		return this.props.slug
+	}
+
+	get attachments() {
+		return this.props.attachments
 	}
 
 	get createdAt() {
@@ -73,14 +79,19 @@ export class Question extends AggregateRoot<QuestionProps> {
 		this.touch()
 	}
 
+	set attachments(attachments: QuestionAttachment[]) {
+		this.props.attachments = attachments
+	}
+
 	static create(
-		props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+		props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>,
 		id?: UniqueEntityId,
 	) {
 		const question = new Question(
 			{
 				...props,
 				slug: props.slug ?? Slug.createFromText(props.title),
+				attachments: props.attachments ?? [],
 				createdAt: props.createdAt ?? new Date(),
 			},
 			id,
