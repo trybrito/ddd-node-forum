599e0b6056df648815f448ebe057581ba37983fb
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-13 23:08:07 -0300

    feat: event created

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -2,6 +2,7 @@ import type { Optional } from '@/core/@types/optional'
 import { AggregateRoot } from '@/core/entities/aggregate-root'
 import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { AnswerAttachmentList } from './answer-attachment-list'
+import { AnswerCreatedEvent } from '../events/answer-created-event'
 
 export interface AnswerProps {
 	questionId: UniqueEntityId
@@ -67,6 +68,12 @@ export class Answer extends AggregateRoot<AnswerProps> {
 			},
 			id,
 		)
+
+		const isNewAnswer = !id
+
+		if (isNewAnswer) {
+			answer.addDomainEvent(new AnswerCreatedEvent(answer))
+		}
 
 		return answer
 	}
